3da25e2345bc580a622308271247957df2e422ae
Joseph Gatto <35414926+JosephGatto@users.noreply.github.com>
2023-12-06 16:48:55 -0500

    Update EAEtrainer.py

--- TextEE/models/PAIE/EAEtrainer.py
+++ TextEE/models/PAIE/EAEtrainer.py
@@ -23,8 +23,7 @@ def EAE_collate_fn(batch):
         batch_token_num=[instance["token_num"] for instance in batch], 
         batch_text=[instance["text"] for instance in batch], 
         batch_trigger=[instance["trigger"] for instance in batch], 
-        batch_arguments=[instance["arguments"] for instance in batch],
-        batch_token_snt_map=[instance["token_snt_map"] for instance in batch],
+        batch_arguments=[instance["arguments"] for instance in batch], 
     )
 
 class PAIEEAETrainer(BasicTrainer):
@@ -71,7 +70,7 @@ class PAIEEAETrainer(BasicTrainer):
                       "text": dt["text"], 
                       "trigger": dt["trigger"], 
                       "arguments": dt["arguments"],
-                      "token_snt_map": dt["token_snt_map"],
+                      
                      }
             
             new_data.append(new_dt)
@@ -169,15 +168,15 @@ class PAIEEAETrainer(BasicTrainer):
                                                      shuffle=False, collate_fn=EAE_collate_fn)):
             progress.update(1)
             batch_pred_arguments = self.model.predict(batch)
-            for doc_id, wnd_id, tokens, text, trigger, pred_arguments, token_snt_map in zip(batch.batch_doc_id, batch.batch_wnd_id, batch.batch_tokens, batch.batch_text, 
-                                                                             batch.batch_trigger, batch_pred_arguments, batch.batch_token_snt_map):
+            for doc_id, wnd_id, tokens, text, trigger, pred_arguments in zip(batch.batch_doc_id, batch.batch_wnd_id, batch.batch_tokens, batch.batch_text, 
+                                                                             batch.batch_trigger, batch_pred_arguments):
                 prediction = {"doc_id": doc_id,  
                               "wnd_id": wnd_id, 
                               "tokens": tokens, 
                               "text": text, 
                               "trigger": trigger, 
                               "arguments": pred_arguments,
-                              "token_snt_map":token_snt_map,
+                              
                              }
                 
                 predictions.append(prediction)
