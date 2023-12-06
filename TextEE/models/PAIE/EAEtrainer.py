276d6e64b70c0e34345a420a82950641b136f3ad
Joseph Gatto <35414926+JosephGatto@users.noreply.github.com>
2023-12-06 16:31:55 -0500

    Update EAEtrainer.py

--- TextEE/models/PAIE/EAEtrainer.py
+++ TextEE/models/PAIE/EAEtrainer.py
@@ -69,7 +69,8 @@ class PAIEEAETrainer(BasicTrainer):
                       "token_num": len(dt["tokens"]), 
                       "text": dt["text"], 
                       "trigger": dt["trigger"], 
-                      "arguments": dt["arguments"]
+                      "arguments": dt["arguments"],
+                      "token_snt_map": dt["token_snt_map"]
                      }
             
             new_data.append(new_dt)
@@ -167,14 +168,15 @@ class PAIEEAETrainer(BasicTrainer):
                                                      shuffle=False, collate_fn=EAE_collate_fn)):
             progress.update(1)
             batch_pred_arguments = self.model.predict(batch)
-            for doc_id, wnd_id, tokens, text, trigger, pred_arguments in zip(batch.batch_doc_id, batch.batch_wnd_id, batch.batch_tokens, batch.batch_text, 
-                                                                             batch.batch_trigger, batch_pred_arguments):
+            for doc_id, wnd_id, tokens, text, trigger, pred_arguments, token_snt_map in zip(batch.batch_doc_id, batch.batch_wnd_id, batch.batch_tokens, batch.batch_text, 
+                                                                             batch.batch_trigger, batch_pred_arguments, batch.batch_token_snt_map):
                 prediction = {"doc_id": doc_id,  
                               "wnd_id": wnd_id, 
                               "tokens": tokens, 
                               "text": text, 
                               "trigger": trigger, 
-                              "arguments": pred_arguments
+                              "arguments": pred_arguments,
+                              "token_snt_map": token_snt_map,
                              }
                 
                 predictions.append(prediction)
